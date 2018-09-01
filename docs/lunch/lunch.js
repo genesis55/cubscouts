87383e58e812ac132e541c55610d8d17cae24cd2
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2018-09-01 16:00:56 -0400

    Added full jquery

--- docs/lunch/lunch.js
+++ docs/lunch/lunch.js
@@ -1,25 +1,20 @@
 $(document).ready(function() {
-  myFunction();
-  getLunch();
+	myFunction();
+	getLunch();
 });
 
 function myFunction() {
-  alert('hi');
-  $('#my_div').text('hello');
+	$('#my_div').text('hello');
 }
 
 function getLunch() {
 	var mc_elementary_lunch_url = "https://www.montgomeryschoolsmd.org/departments/foodserv/menus/elementary_lunch.aspx";
-	  $('#my_div').append('div').text(mc_elementary_lunch_url);
-	  $.get(mc_elementary_lunch_url, function(response) {
-  		console.log(response);
-	  });
+	$('#my_div').append('div').text(mc_elementary_lunch_url);
 
-var name = "codemzy";
-var url = "http://anyorigin.com/go?url=" + encodeURIComponent("https://genesis55.gihub.pages/") + name + "&callback=?";
-$.get(url, function(response) {
-  console.log(response);
-});	  
+	var url = "http://anyorigin.com/go?url=" + encodeURIComponent(mc_elementary_lunch_url) + "&callback=?";
+	$.get(url, function(response) {
+	  console.log(response);
+	});	  
 }
 
 
